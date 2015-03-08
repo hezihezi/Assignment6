c7cdd9116e9acd71abb9fab5da5279300087cab1
hezihezi <hj2393@columbia.edu>
2015-03-08 15:24:11 -0400

    added an OpenStreetMap tile layer

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 var map = L.map('healthCenterMap').setView([40.714269,-74.005973], 13);
 
+// add an OpenStreetMap tile layer
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
