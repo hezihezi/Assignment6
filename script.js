b649f184ff698887b9e79a8568231364f5b63c2f
hezihezi <hj2393@columbia.edu>
2015-03-08 16:43:15 -0400

    added pop-up to the markers. name of center and full address

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-var map = L.map('healthCenterMap').setView([40.714269,-74.005973], 13);
+var map = L.map('healthCenterMap').setView([40.714269, -74.005973], 13);
 
 // add an OpenStreetMap tile layer
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	attribution : '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 var mappingDataArray = mappingData.rows;
 
-for (var i=0; i<mappingDataArray.length; i++) {
+for (var i = 0; i < mappingDataArray.length; i++) {
 	var locationObject = mappingDataArray[i];
-	
-	var marker = L.marker([locationObject.latitude,locationObject.longitude]).addTo(map);
-	
+
+	var marker = L.marker([locationObject.latitude, locationObject.longitude]).addTo(map);
+
+	marker.bindPopup("<b>" + locationObject.NameofCenter + "</b><br>" + locationObject.fullAddress);
 
 }
-
 
